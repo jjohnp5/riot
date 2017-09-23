aaf01ba7ee8ca244401ba63f4fb8e0dab3fecc81
John <jjohnp5@gmail.com>
2017-09-23 14:25:42 -0500

    updated ajax calls to grab images for items, champions and spells. displaying correct data to the DOM now with correct images. will add more string data in the future for calculations

--- server.js
+++ server.js
@@ -17,6 +17,8 @@ dotenv.config({path: '.env.text'});
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("views", path.join(__dirname, '/views'));
+app.use(express.static(__dirname + "/public"));
+
 
 app.get('/', function(req, res){
     res.render('index');
@@ -44,23 +46,44 @@ app.get("/matches/:id", function(req, res){
     request("https://na1.api.riotgames.com/lol/match/v3/matches/" + req.params.id + "?api_key=" + process.env.API_KEY, function (err, response, match) {
         if (!err && response.statusCode === 200) {
                 data = JSON.parse(match);
-                console.log(data);
                 res.send(data);
             }else {
             res.sendStatus(response.statusCode);
         }
         })
 });
-// function callAPI(url){
-//     return new Promise((resolve, reject) => {
-//
-//             .then(res => JSON.parse(res))
-//             .then(data => resolve(data))
-//             .catch(err = >)
-// }
-//         })
-// }
-//
+
+app.get('/summoner/spells', function (req, res) {
+    request("http://ddragon.leagueoflegends.com/cdn/"+process.env.PATCH_VERSION+"/data/en_US/summoner.json", function (err, response, spells) {
+        if(!err && response.statusCode === 200){
+            spell = JSON.parse(spells)
+            res.send(spell);
+        }else{
+            res.sendStatus(response.statusCode)
+            console.log(response.statusCode)
+        }
+    })
+})
+app.get('/summoner/champs', function (req, res) {
+    request("http://ddragon.leagueoflegends.com/cdn/"+process.env.PATCH_VERSION+"/data/en_US/champion.json", function (err, response, champs) {
+        if(!err && response.statusCode === 200){
+            champ = JSON.parse(champs);
+            res.send(champ)
+        }else{
+            res.sendStatus(response.statusCode);
+        }
+    })
+})
+app.get('/summoner/items', function (req, res) {
+    request("http://ddragon.leagueoflegends.com/cdn/"+process.env.PATCH_VERSION+"/data/en_US/item.json", function (err, response, champs) {
+        if(!err && response.statusCode === 200){
+            champ = JSON.parse(champs);
+            res.send(champ)
+        }else{
+            res.sendStatus(response.statusCode);
+        }
+    })
+})
 
 app.listen(process.env.PORT || 3000, function () {
    console.log("app started");
