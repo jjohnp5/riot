e197ac1165e7d08ffa132c4c84c615f250277adf
John <jjohnp5@gmail.com>
2017-10-23 23:28:18 -0500

    added builds route, added mongoose, passport, passport-local, passport-local-mongoose. added schemas and models for guides. created ejs files for the specific routes.

--- server.js
+++ server.js
@@ -8,9 +8,67 @@ var axios = require('axios');
 var dotenv = require('dotenv');
 var queues = require('./queues.json');
 var session = require('express-session');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var localStrategy = require('passport-local');
+var passportLocalMongoose = require('passport-local-mongoose');
 var sessionHolder;
 
+//dummy data
 
+
+data = {
+    title: "data1",
+    champion: "datachamp",
+    items: [123, 234],
+    spells: [1234, 1234],
+    skills: [123, 123, 1234, 1243],
+    chapters: [
+        { 	title: "chapter1",
+            content: "content 1"
+        }
+    ],
+    author: {id: "1234", username: "name"}
+};
+/*
+    Database schemas
+
+ */
+//Connect to db
+mongoose.connect("mongodb://localhost/riotdb");
+
+var userSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+userSchema.plugin(passportLocalMongoose);
+var User = mongoose.model('User', userSchema);
+
+
+//ItemID, SpellID,
+var guideSchema = new mongoose.Schema({
+    title: String,
+    champion: String,
+    items: [Number],
+    spells: [Number],
+    skills: [Number],
+    chapters: [{title: String, content: String}],
+    author:{
+        id: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User"
+        },
+        username: String
+    }
+})
+var Guides = mongoose.model('Guides', guideSchema);
+Guides.create(data, function (err, guide) {
+    if(err){
+        console.log(err);
+    }else{
+        console.log(guide);
+    }
+})
 
 dotenv.config({path: '.env'});
 
@@ -19,6 +77,17 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.set("views", path.join(__dirname, '/views'));
 app.use(express.static(__dirname + "/public"));
 app.use(session({secret: 'riotimizer', resave: false, saveUninitialized: true}));
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+app.use(function (req, res, next) {
+    res.locals.currentUser = req.user;
+})
+
+//seedDB
+
 
 /*
     *
@@ -143,6 +212,88 @@ app.get('/calculator', isSessionExists,function(req,res) {
     });
 });
 
+
+/*
+    Start Collab ROUTES here, we will use a different middle ware later. for now, we'll be using session middleware.
+
+ */
+
+// REGISTER ROUTE
+app.get('/register', isSessionExists, function (req, res) {
+    res.render('register');
+})
+
+app.post('register', function (req, res) {
+    var newUser = new User({username: req.body.username});
+    User.register(newUser, req.body.password, function (err, user) {
+        if(err){
+            return res.redirect("/register");
+        }
+        passport.authenticate('local')(req, res, function () {
+            res.redirect('/builds');
+        })
+
+    })
+})
+
+//LOGIN ROUTE
+app.get('/login', function (req, res) {
+    res.render('login');
+})
+
+app.post('/login', passport.authenticate('local', {
+    successRedirect: "/builds",
+    failureRedirect: "/login"
+}), function (req, res) {
+    });
+
+//LOGOUT ROUTE
+app.get('/logout', function (req, res) {
+    req.logout();
+    res.redirect('/builds');
+})
+
+/*
+    BUILDS ROUTE, This is where we display the builds. We store builds in the database and render them to users.
+    We will still use data gathered from RIOT api like item IDs and Spells, and champions.
+    We will do the refactoring later. All views are currently in the main views folder, we will take care of this at
+    a later time.
+ */
+app.get('/builds', function (req, res) {
+    Guides.find({}, function(err, guide){
+        if(err){
+            console.log(err);
+        }else{
+            res.render('builds', {guides: guide});
+        }
+    })
+})
+//We will add middleware later once authentication, I'm just building routes at the moment. Running low on time.
+//Items will be sent as JSON.stringify and parsed as JSON on the backend.
+app.post('/builds', function (req, res) {
+    var title = req.title;
+    var champion = req.champion;
+    var items = JSON.parse(req.items);
+    var spells = JSON.parse(req.spells);
+    var skills = JSON.parse(req.skills);
+    var chapters = JSON.parse(req.chapters);
+    var author = {
+        id: req.user._id,
+        username: req.user.username
+    }
+    var newGuide = {title: title, champion: champion, items: items, spells: spells, skills: skills, chapters: chapters, author: author};
+    Guides.create(newGuide, function (err, guide) {
+        if(err){
+            console.log('failed to add to db');
+            console.log(err);
+        }else{
+            res.redirect('/builds');
+        }
+    })
+
+})
+
+
 /*
  *  Middleware for session. need to be added to main routes to prevent API abuse. No need to add this to
  *  /summoner + /items, /spells, /champs
@@ -158,6 +309,8 @@ function isSessionExists(req, res, next){
     }
     res.render("unauthenticated");
 }
+
+
 
 
 app.listen(process.env.PORT || 3000, function () {
